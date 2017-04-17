36056a6aa6b98bba6f4f676f999f6d6d04052c79
Charles Wooley <cwooley89@gmail.com>
2017-04-17 22:53:47 +0000

    added Users table, and implemented confirmation email through sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,13 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  
+  
+  config.action_mailer.delivery_method = :test
 
+  config.action_mailer.default_url_options = { :host => 'https://dissecting-rails-wooleymammoth.c9users.io/spots'}
+  
+  
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
